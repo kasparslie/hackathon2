d2e1e2226739293a397b27ee9d605b1857402d1d
Kaspars Liepins <kasparslie@gmail.com>
2021-01-22 12:23:59 +0100

    revenue calculator commented out

--- src/Components/RecordTime.tsx
+++ src/Components/RecordTime.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { ChoiceGroup, IChoiceGroupOption} from 'office-ui-fabric-react/lib/ChoiceGroup';
 import axios from 'axios';
+import "./RecordTime.css"
 
 
 function RecordTime() {
@@ -34,6 +35,7 @@ function RecordTime() {
     //      const rate=parseInt(data.rate)
     //      const time=parseInt(data.time)
     //     let totalVal=(rate*time)
+       //   setTotal(totalVal)
     //   }
 
     
@@ -43,30 +45,38 @@ function RecordTime() {
  
      
     return(
-    <div>
+    <div className="main">
    <form onSubmit={postData}>
+     <div className="f1">
    <TextField placeholder="descrpition" type="text" id="descrpition" value={data.descrpition} 
    onChange={(e, value) => setData({ ...data, descrpition: value })}></TextField>
+    <div className="f1a">
       <TextField placeholder="file" type="text" id="file" value={data.file} 
    onChange={(e, value) => setData({ ...data, file: value })}></TextField>
+   </div>
+   </div>
+   <div className="f2">
       <TextField placeholder="time" type="text" id="time" value={data.time} 
    onChange={(e, value) => setData({ ...data, time: value })}></TextField>
          <TextField placeholder="rate" type="number" id="rate" value={data.rate} 
    onChange={(e, value) => setData({ ...data, rate: value })}></TextField>
            <TextField placeholder="revenue" type="number" id="revenue" readOnly value={data.revenue} 
    onChange={(e, value) => setData({ ...data, revenue: value })}></TextField>
+   </div>
          <TextField placeholder="performer" type="text" id="performer" value={data.performer} 
    onChange={(e, value) => setData({ ...data, performer: value })}></TextField>
               <TextField placeholder="dd-mm-yyyy" type="date" id="date" value={data.date} 
    onChange={(e, value) => setData({ ...data, date: value })}></TextField>
+     <div className="f3">
      <ChoiceGroup defaultSelectedKey="B" options={options} label="Pick one" required={true}
      onChange={_onChange}
     />; 
      {console.log(options)}
 {/* {*{onChange={(e, value) => setData({ ...data, value)} } *} */}
-
-
+ </div>
+<div className="f4">
    <button type="submit">save</button>
+</div>
    </form>
    
 
